4526f482041640cc33217407bd50557aa8b44864
Davide Baldelli <davbaldelli@gmail.com>
2022-03-17 17:25:06 +0100

    minor

--- scripts/brandsFormatter.js
+++ scripts/brandsFormatter.js
@@ -1,11 +1,13 @@
 export function brandsToCards(brands){
     return brands.map(brand => {
-        return `<a href="brand.php?name=${brand.name}"><div class="card m-3 card-brand" data-key="${brand.name}">
-                    <img src="${brand.logo}" class="card-img-top card-image-fit p-2" alt="brand logo">
-                    <div class="card-body">
-                        <h5 class="card-title text-center"> ${brand.name} </h5>
+        return `<a href="brand.php?name=${brand.name}">
+                    <div class="card m-3 card-brand" data-key="${brand.name}">
+                        <img src="${brand.logo}" class="card-img-top card-image-fit p-2" alt="brand logo">
+                        <div class="card-body">
+                            <h5 class="card-title text-center"> ${brand.name} </h5>
+                        </div>
                     </div>
-                </div></a>`
+                </a>`
     })
 }
 
