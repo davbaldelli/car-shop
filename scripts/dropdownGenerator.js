24f9921486ce70e46d8efb7d48fed4d646f79ca8
kicherS <90626419+kicherS@users.noreply.github.com>
2022-03-17 18:36:54 +0100

    changes in drpgen

--- scripts/dropdownGenerator.js
+++ scripts/dropdownGenerator.js
@@ -1,11 +1,11 @@
-
 //prende in input il nome del bottone e restituisce l'html percui usarlo $(tag).html(creaDropdownBtn(nome))
-export function creaDropdownBtn(nomebtn){
+export function creaDropdownBtn(nomebtn, codice){
     return `<div class="dropdown" id="dropdown-me">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     ${nomebtn}
                 </button>
                 <div class="dropdown-menu dropdown-multicol" aria-labelledby="dropdownMenuButton">
+                    ${codice}
                 </div>
             </div>
             `
@@ -24,17 +24,14 @@ export function creaDropdownItems(oggetti){
 
 //prende l' arrey di creaDropdownItems e il numero di colonne retituendo l'html 
 export function popolaDropdownContent(items, column){
-    let tmp=0
     let a=""       
-    for(let i=0; i<column; i++){    
-        let b=""
-        for(let j=0; j<(items.length/column); j++){
-            b = b + items[tmp] 
-            
-            tmp++
+        for(let i=0, tmp=0; i<column; i++){    
+            let b=""
+            for(let j=0; j<(items.length/column) && tmp<items.length; j++,tmp++){
+                b = b + items[tmp] 
+            }
+            a = a +`<div class="dropdown-col" >${b}</div>` 
         }
-        a = a +`<div class="dropdown-col" >${b}</div>` 
-    }
     return(a)
 }
 
@@ -50,4 +47,25 @@ export function aggiungiCssInHead(){
             width: auto;
         }
     </style>`)
+}
+
+
+function generateDropdown(nome, elementi, colonne, onChange, onSelected, onUnselected){
+            
+    creaDropdownBtn(nome, popolaDropdownContent(creaDropdownBtn(elementi), colonne))
+            
+    $(".dropdown-item").click(event => {
+        onSelected(event.currentTarget.dataset.key)
+        let prev = state
+        state.push(event.currentTarget.dataset.key)
+        onChange(prev, state)
+    })
+
+    $(".dropdown-item").unClick(event => {
+        onUnselected(event.currentTarget.dataset.key)
+        let tmp = state
+        state.filter(e => e !== event.currentTarget.dataset.key)
+        onChange(tmp, state)
+    })
+
 }
